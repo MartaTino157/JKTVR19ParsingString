a7d80f8b3821242443a7f332d34dc4c4b8d0ad73
olesia gusarova <olesia.gusarova@ivkhk.ee>
2020-09-25 13:40:34 +0300

    Работает первое

--- src/jktvr19parsingstring/App.java
+++ src/jktvr19parsingstring/App.java
@@ -5,13 +5,57 @@
  */
 package jktvr19parsingstring;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
  */
 class App {
     public void run(){
-        System.out.println("---- Работа со строкой ----");
+        System.out.println("-------------- Работа со строкой --------------");
+        System.out.println("------------------------------------------------");
+        System.out.println("Список задач: ");
+        System.out.println("1. Посчитать количесиво слов в строке");
+        System.out.println("2. Найти указанную подстроку в исходной строке");
+        System.out.println("3. Заменить исходную строку строку на указанную");
+        System.out.println("4. Удалить все теги из html строки");
+        System.out.println("5. Удалить конкретный тег");
+        System.out.println("------------------------------------------------");
+        System.out.println("Введите исходную строку");
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        System.out.println("------------------------------------------------");
+        System.out.println("Выберите номер задачи: ");
+        String task = scan.nextLine();    
+        switch (task) {
+            case "1":
+                System.out.println("Вы выбрали: Посчитать количесиво слов в строке");
+                System.out.println("Количество слов в строке "+ this.getCountWords(str));
+                break;
+            case "2":
+                System.out.println("Вы выбрали: Найти указанную подстроку в исходной строке");
+                break;
+            case "3":
+                System.out.println("Вы выбрали: Заменить исходную строку строку на указанную");
+                break;
+            case "4":
+                System.out.println("Вы выбрали: Удалить все теги из html строки");
+                break;
+            case "5":
+                System.out.println("Вы выбрали: Удалить конкретный тег");
+                break;
+            default:
+                System.out.println("НЕТ такой задачи");
+        }
+            
+    }
+    private int getCountWords(String str) {
+        if(str.isEmpty()) {
+            return 0;
+        }
+        String[] arrStr = str.split(" ");
+        return arrStr.length;
     }
     
 }
